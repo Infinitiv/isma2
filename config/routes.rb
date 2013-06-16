26f696985079ab1fef2dd181b287991b5f1bad6c
Vladimir Markovnin <markovnin@gmail.com>
2013-06-16 20:58:53 +0400

    Правка маршрутов

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Isma2::Application.routes.draw do
-  resources :attachments
 
-  resources :articles
+  resources :articles do
+    resources :comments, :attachments
+  end
 
-  resources :comments
-
+  controller :articles do
+    get :feed
+  end
+  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    get 'logout' => :destroy
+  end
+  
+  controller :archives do
+    get '/archives/articles' => :articles
+    get '/archives/news' => :news
+    get '/archives/anounces' => :anounces
+    get '/archives/all' => :all
+  end
+  
   resources :groups
 
   resources :posts
@@ -14,19 +30,23 @@ Isma2::Application.routes.draw do
   resources :profiles
 
   resources :users
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  
+  root 'articles#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+  get 'attachments/:id/minify_img' => 'images#minify_img', :as => :minify_img
+  
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
